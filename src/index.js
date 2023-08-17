1b34e0f9517472c4e405719126117bdb718e1904
Daniela Jacome <jacomedanny1@gmail.com>
2023-08-17 17:00:51 -0500

    logo update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-console.log('Happy hacking :)')
+import {registerImage} from '../src/lazy'
+
+const minium = 1;
+const maxium = 122;
+const baseUrl="https://randomfox.ca/images/";
+
+const random = () => Math.floor ( Math.random() * (maxium - minium)) + minium
+
+const createImageNode= () =>{
+    const container = document.createElement("div");
+    container.className = "p-4";
+
+    const imagen=document.createElement("img");
+    imagen.className ="mx-auto";
+    imagen.width="320";
+    imagen.dataset.src=`${baseUrl}${random()}.jpg`;
+    
+    container.appendChild(imagen);
+
+    return imagen;
+};
+
+const mounNode=document.getElementById("images");
+
+const addButton =document.getElementById("add");
+const addImage = () => {
+    const newImage = createImageNode();
+    mounNode.append(newImage);
+    registerImage(newImage);
+}
+
+const removeButton =document.getElementById("clean");
+const removeImage = () => {
+    while (mounNode.firstChild) {
+        mounNode.removeChild(mounNode.firstChild);
+    }
+}
+
+addButton.addEventListener("click",addImage);
+removeButton.addEventListener("click",removeImage);
---
+++ src/lazy.js
@@ -0,0 +1,28 @@
+
+const isIntersecting = (entry) =>{
+    //funcion importante porq se puede usar si pej esta a 200 px hacer x o y cosa
+    return entry.isIntersecting //true dentro de la pantalla
+}
+
+const accion = (entry) => { //carga imagen
+
+    const container = entry.target; //es un contenedor (div), aqui esta el data-src
+    const url = container.dataset.src; // devuelve un img
+    //const url = imagen.dataset.src;
+    //load image
+    container.src=url;
+    console.log("holis");
+
+    //desregistra la imagen (unlisten)
+    observer.unobserve(container);
+};
+
+//new intersectionObserver(funcionQueHacerPorImagen)
+const observer = new IntersectionObserver((entries)=>{
+    entries.filter(isIntersecting).forEach(accion)
+})
+
+export const registerImage = (imagen) => {
+    //Intersection Observador -> observer(imagen)
+    observer.observe(imagen)
+};
