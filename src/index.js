8731845ca7a8c361012dc50cf70d102321d71c47
Daniela Jacome <jacomedanny1@gmail.com>
2023-08-18 14:24:16 -0500

    update publish

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
-
-import {registerImage} from '../src/lazy'
+import { registerImage } from "../src/lazy";
 
 const minium = 1;
 const maxium = 122;
-const baseUrl="https://randomfox.ca/images/";
+const baseUrl = "https://randomfox.ca/images/";
 
-const random = () => Math.floor ( Math.random() * (maxium - minium)) + minium
+const random = () => Math.floor(Math.random() * (maxium - minium)) + minium;
 
-const createImageNode= () =>{
-    const container = document.createElement("div");
-    container.className = "p-4";
+const createImageNode = () => {
+  const container = document.createElement("div");
+  container.className = "p-4";
 
-    const imagen=document.createElement("img");
-    imagen.className ="mx-auto";
-    imagen.width="320";
-    imagen.dataset.src=`${baseUrl}${random()}.jpg`;
-    
-    container.appendChild(imagen);
+  const imagen = document.createElement("img");
+  imagen.className = "mx-auto";
+  imagen.width = "320";
+  imagen.dataset.src = `${baseUrl}${random()}.jpg`;
 
-    return imagen;
+  container.appendChild(imagen);
+
+  return imagen;
 };
 
-const mounNode=document.getElementById("images");
+const mounNode = document.getElementById("images");
 
-const addButton =document.getElementById("add");
+const addButton = document.getElementById("add");
 const addImage = () => {
-    const newImage = createImageNode();
-    mounNode.append(newImage);
-    registerImage(newImage);
-}
+  const newImage = createImageNode();
+  mounNode.append(newImage);
+  registerImage(newImage);
+};
 
-const removeButton =document.getElementById("clean");
+const removeButton = document.getElementById("clean");
 const removeImage = () => {
-    while (mounNode.firstChild) {
-        mounNode.removeChild(mounNode.firstChild);
-    }
-}
+  while (mounNode.firstChild) {
+    mounNode.removeChild(mounNode.firstChild);
+  }
+};
 
-addButton.addEventListener("click",addImage);
-removeButton.addEventListener("click",removeImage);
+addButton.addEventListener("click", addImage);
+removeButton.addEventListener("click", removeImage);
